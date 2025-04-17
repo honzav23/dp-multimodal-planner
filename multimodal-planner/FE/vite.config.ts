710c96df925fd0512bd1fc15fb0bf50b0b3253a8
honzav23 <janvaclavik@valachnet.cz>
2025-04-17 09:48:06 +0200

    Getting ready for proper dockerization

--- multimodal-planner/FE/vite.config.ts
+++ multimodal-planner/FE/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
     base: '/',
+    root: './',
     plugins: [react()],
     build: {
         commonjsOptions: { transformMixedEsModules: true }
